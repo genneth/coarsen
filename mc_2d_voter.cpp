b1402ca9e5ca43473e978620e72c9736dd45a7e0
Gen Zhang <genneth@gmail.com>
2010-03-12 14:04:33 +0000

    refactor to have a grid type and fix neighbour choice

--- mc_2d_voter.cpp
+++ mc_2d_voter.cpp
@@ -4,6 +4,36 @@
 
 boost::mt19937 rng;
 
+template <size_t N>
+struct grid_lattice {
+	explicit grid_lattice(): g({{0}}) {g[N/2][N/2] = 1;}
+
+	unsigned int operator()(int i, int j) const {return g[i][j];}
+	unsigned int & operator()(int i, int j) {return g[i][j];}
+
+	size_t size() const {
+		size_t s = 0;
+		for(size_t i = 0; i < N; ++i) {
+			for(size_t j = 0; j < N; ++j)
+				s += g[i][j];
+		}
+		return s;
+	}
+private:
+	unsigned int g[N][N];
+};
+
+template <size_t N>
+std::ostream & operator<<(std::ostream & os, const grid_lattice<N> & g)
+{
+	for(unsigned int i = 0; i < N; ++i) {
+		for(unsigned int j = 0; j < N; ++j)
+			os << g(i,j) << " ";
+		os << std::endl;
+	}
+	return os;
+}
+
 int main()
 {
 	using namespace std;
@@ -22,31 +52,24 @@ int main()
 		neighbour(rng, direction);
 
 	for(int count = 0; count < 10000;) {
-		unsigned int grid[N][N] = {{0}};
-		grid[N/2][N/2] = 1;
+		grid_lattice<N> grid;
 		double t = 0.0;
 		do {
 			double dt = next_event();
 			unsigned int x = next_x(), y = next_y();
 
 			int dir = neighbour();
-			int dx = (dir & 1) * 2 - 1;
-			int dy = (dir & 2) - 1;
+			int dx =    (dir & 1)  * ((dir & 2) - 1);
+			int dy = (1-(dir & 1)) * ((dir & 2) - 1);
 
-			grid[x][y] = grid[(x+dx)%N][(y+dy)%N];
+			grid(x, y) = grid((x+dx)%N, (y+dy)%N);
 
 			t += dt;
-			// cout << t << " " << x << " " << y << " " << dx << " " << dy << endl;
 		} while(t < 10.0);
 
-		size_t size = 0;
-		for(size_t i = 0; i < N; ++i) {
-			for(size_t j = 0; j < N; ++j) {
-				size += grid[i][j];
-			}
-		}
-		if(size > 0) {
-			cout << size << endl;
+		if(grid.size() > 0) {
+			cout << grid.size() << endl;
+			cout << grid << endl;
 			count++;
 		}
 	}
