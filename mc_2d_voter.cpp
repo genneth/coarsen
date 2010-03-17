3e29fb81e723d0855fd81384bff72138a0647588
Gen <genneth@gmail.com>
2010-03-17 16:38:42 +0000

    fix boundary

--- mc_2d_voter.cpp
+++ mc_2d_voter.cpp
@@ -7,15 +7,15 @@
 
 boost::mt19937 rng(static_cast<uint32_t>(::time(0)));
 
-template <size_t N>
+template <int N>
 struct grid_lattice {
 	explicit grid_lattice(): time(0.0), g({{0}}) {
 		active[std::make_pair(N/2,N/2)] = 0;
 		set(N/2, N/2, 1);
 	}
 
-	unsigned int cell(size_t i, size_t j) const {return g[i%N][j%N];}
-	void set(size_t i, size_t j, unsigned int o) {
+	unsigned int cell(int i, int j) const {sanitise(i,j); return g[i][j];}
+	void set(int i, int j, unsigned int o) {
 		// precondition: active.find(make_pair(i,j)) != active.end();
 		// also, if cell(i,j) then its neighbours are in active
 
@@ -38,14 +38,14 @@ struct grid_lattice {
 		}
 	}
 
-	size_t labelled_neighbours(size_t i, size_t j) const {
+	int labelled_neighbours(int i, int j) const {
 		return cell(i+1,j) + cell(i-1,j) + cell(i,j+1) + cell(i,j-1);
 	}
 
-	size_t size() const {
-		size_t s = 0;
-		for(size_t i = 0; i < N; ++i) {
-			for(size_t j = 0; j < N; ++j)
+	int size() const {
+		int s = 0;
+		for(int i = 0; i < N; ++i) {
+			for(int j = 0; j < N; ++j)
 				s += g[i][j];
 		}
 		return s;
@@ -80,25 +80,28 @@ struct grid_lattice {
 
 private:
 	unsigned int g[N][N];
-	typedef std::map<std::pair<size_t,size_t>, unsigned int> active_list_t;
+	typedef std::map<std::pair<int,int>, unsigned int> active_list_t;
 	active_list_t active;
 
-	unsigned int & cell_(size_t i, size_t j) {return g[i%N][j%N];}
-	void inc_neighbours(size_t i, size_t j) {
+	void sanitise(int & i, int & j) const {i = (i+N)%N; j = (j+N)%N;} // positive dividend
+	unsigned int & cell_(int i, int j) {sanitise(i,j); return g[i][j];}
+	void inc_neighbours(int i, int j) {
+		sanitise(i,j);
 		active_list_t::iterator c = active.find(std::make_pair(i,j));
 		if(c == active.end())
 			active[std::make_pair(i,j)] = 1;
 		else
 			c->second++;
 	}
-	void dec_neighbours(size_t i, size_t j) {
+	void dec_neighbours(int i, int j) {
+		sanitise(i,j);
 		active_list_t::iterator c = active.find(std::make_pair(i,j));
 		if(--c->second == 0 && cell(i,j) == 0)
 			active.erase(c);
 	}
 };
 
-template <size_t N>
+template <int N>
 std::ostream & operator<<(std::ostream & os, const grid_lattice<N> & g)
 {
 	for(unsigned int i = 0; i < N; ++i) {
@@ -114,7 +117,7 @@ int main()
 	using namespace std;
 	using namespace boost;
 
-	const size_t N = 101;
+	const int N = 101;
 
 	for(int count = 0; count < 1;) {
 		grid_lattice<N> grid;
@@ -122,7 +125,7 @@ int main()
 			double dt = grid.next_event();
 			grid.flip();
 			grid.time += dt;
-		} while(grid.time < 50.0 && !grid.empty());
+		} while(grid.time < 500.0 && !grid.empty());
 
 		if(!grid.empty()) {
 			cout << grid.size() << endl;
